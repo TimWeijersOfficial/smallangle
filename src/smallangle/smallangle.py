5c39dcf261f66f0f2d81db9292046ee00245742a
Tim Weijers <timweijers01@gmail.com>
2024-11-14 12:30:14 +0100

    click subcommando's gemaakt

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -3,18 +3,31 @@ import numpy as np
 from numpy import pi
 import pandas as pd
 
+@click.group()
+def angle_group():
+    pass
 
-def sin(number):
-    x = np.linspace(0, 2 * pi, number)
+@angle_group.command()
+@click.option(
+"-n",
+"--steps",
+default=10,
+)
+def sin(steps):
+    x = np.linspace(0, 2 * pi, steps)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
-
-def tan(number):
-    x = np.linspace(0, 2 * pi, number)
+@angle_group.command()
+@click.option(
+"-n",
+"--steps",
+default=10
+)
+def tan(steps):
+    x = np.linspace(0, 2 * pi, steps)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
-
 
 if __name__ == "__main__":
     sin(10)
