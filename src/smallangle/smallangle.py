0e6aaba442894aa147d12ac9536b48eaec324064
Tim Weijers <timweijers01@gmail.com>
2024-11-14 12:40:15 +0100

    docstrings toegevoegd

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -5,6 +5,9 @@ import pandas as pd
 
 @click.group()
 def angle_group():
+    """prints sine or tangent values (depending on subcommand)
+    of values between 0 and pi
+    """    
     pass
 
 @angle_group.command()
@@ -14,6 +17,12 @@ def angle_group():
 default=10,
 )
 def sin(steps):
+    """prints table of values between 0 and pi
+    and their sine values
+
+    Args:
+        steps (int): amount of values between 0 and pi
+    """    
     x = np.linspace(0, 2 * pi, steps)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
@@ -25,6 +34,12 @@ def sin(steps):
 default=10
 )
 def tan(steps):
+    """prints table of values between 0 and pi 
+        and their tangent values
+
+    Args:
+        steps (int): amount of values between 0 and pi
+    """    
     x = np.linspace(0, 2 * pi, steps)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
